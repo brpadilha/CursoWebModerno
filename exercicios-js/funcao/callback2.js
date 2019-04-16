adb14a6aca888a33f2ca12500272952d0af0f30d
bpadilha <padilha.bpr@gmail.com>
2019-04-16 15:03:45 -0300

    Aula 74 - Callback 2

---
+++ exercicios-js/funcao/callback2.js
@@ -0,0 +1,25 @@
+const notas = [7.7, 8.8 , 5.2 , 6.5 , 3.6 , 9.0]
+
+//sem callback
+
+let notasBaixas = []
+for (let i in notas){
+    if (notas[i] < 7){
+        notasBaixas.push(notas[i])
+    }
+}
+
+console.log(notasBaixas)
+
+
+//console callback
+
+const notasBaixas2 = notas.filter(function (nota){
+    return nota < 7
+})
+
+console.log(notasBaixas2)
+
+const notasBaixas3 = notas.filter(nota => nota < 7)
+
+console.log(notasBaixas3)
