3a8e34fea324a01c3a56a24509e896d5f58a6f44
bpadilha <padilha.bpr@gmail.com>
2019-04-16 17:09:42 -0300

    Aula 77 - Tipos de declaração de função

---
+++ exercicios-js/funcao/tiposDeclaracao.js
@@ -0,0 +1,21 @@
+console.log(soma(3,4))
+
+// function declaration -- pode pedir antes da função estar escrita no código
+
+function soma (x, y){
+    return x + y
+}
+
+//function expression
+
+const sub = function (x,y){
+    return x - y
+
+}
+console.log(sub(4, 3))
+//named function expression - pouco usada
+
+const mult = function mult(x, y){
+    return x * y
+}
+
