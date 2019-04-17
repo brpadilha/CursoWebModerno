fb3555b73012a3aa584eba9c936f6ce14037a913
bpadilha <padilha.bpr@gmail.com>
2019-04-17 17:02:48 -0300

    Aula 84 - modo de fugir do escopo global

---
+++ exercicios-js/funcao/aula 84 - IIFE.js
@@ -0,0 +1,10 @@
+// IIFE -> immediately invoked function expression
+
+//Expressão de função imediatamente invocada
+
+(function () {
+    console.log('Será executado na hora!')
+    console.log('Fugindo do escopo mais abrangente!')
+})()
+
+//foge do escopo global
