fff8a052a5c83a6ac34e4936cdca9b984c4806cc
brpadilha <brpadilha.dev@gmail.com>
2019-05-11 21:57:47 -0300

    DeclarativoXImperativo e concatenar arrays

---
+++ exercicios-js/5-array/Aula117-Concat.js
@@ -0,0 +1,9 @@
+const filhas = ['Valeskah', 'Cibalena']
+const filhos = ['Uóxinton', 'Uesclei']
+
+const todos = filhas.concat(filhos, 'Fulano')
+console.log(todos, filhas, filhos)
+
+console.log(['a', 'b'].concat([1, 2], [3, 4], 5, [
+    [6, 7]
+]))
