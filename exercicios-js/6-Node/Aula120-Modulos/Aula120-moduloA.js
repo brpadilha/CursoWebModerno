423e8785bbead8228ee1c0587bde0a3f2549036c
brpadilha <brpadilha.dev@gmail.com>
2019-05-16 17:10:52 -0300

    Aulas 120 e 121

---
+++ exercicios-js/6-Node/Aula120-Modulos/Aula120-moduloA.js
@@ -0,0 +1,5 @@
+this.ola = 'Fala Pessoal' //metodo de exportar
+
+exports.BemVindo = 'Bem vindo ao node!' //metodo de exportar
+
+module.exports.ateLogo = 'Até proximo exemplo'
---
+++ exercicios-js/6-Node/Aula120-Modulos/Aula120-moduloCliente.js
@@ -0,0 +1,10 @@
+const moduloA = require('./Aula120-moduloA')
+const moduloB = require('./Aula120-moduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.BemVindo)
+console.log(moduloA.ateLogo)
+
+
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
