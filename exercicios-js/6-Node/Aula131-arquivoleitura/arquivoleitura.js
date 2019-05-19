bbe5499618f965e557c25d870adedbd8271d5cef
brpadilha <brpadilha.dev@gmail.com>
2019-05-19 16:13:10 -0300

    Aula 131 e 132

---
+++ exercicios-js/6-Node/Aula131-arquivoleitura/arquivoleitura.js
@@ -0,0 +1,14 @@
+const fs = require('fs')
+
+const caminho = __dirname + '/arquivo.json'
+
+
+//sincrono..
+const conteudo = fs.readFileSync(caminho, 'utf-8')
+console.log(conteudo)
+
+//assincrono...
+fs.readFile(caminho, 'utf-8', (err, conteudo) => {
+    const config = JSON.parse(conteudo)
+    console.log(`${confi.db.host}:${config.db.port}`)
+})
